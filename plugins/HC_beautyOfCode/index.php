7d543b9f28ba01cff73b6991b8efb3871da4e550
Cain Chen <hinablue@gmail.com>
2011-05-19 11:59:23 +0800

    Rollback HC_beautyOfCode SyntaxHighlighter to 2.1.382.

--- plugins/HC_beautyOfCode/index.php
+++ plugins/HC_beautyOfCode/index.php
@@ -38,63 +38,40 @@ function CodeHighLighter_ApplyScripts($mother)
     ob_start();
 ?>
 <script type="text/javascript" src="<?php echo $pluginURL; ?>/scripts/shCore.js"></script>
-<script type="text/javascript" src="<?php echo $pluginURL; ?>/scripts/shAutoloader.js"></script>
 <script type="text/javascript">
 //<![CDATA[
-// script.js
-(function(c,j){var h=j.getElementsByTagName("script")[0],e={},a={},d=false,k=function(){},l="string",b=function(){return Array.every||function(f,p,o){for(var n=0,m=f.length;n<m;++n){if(!p(f[n],n,f)){return d}}return 1}}(),i=function(f,n,m){b(f,function(q,p,o){n(q,p,o);return true},m)};if(j.readyState==null&&j.addEventListener){j.addEventListener("DOMContentLoaded",function g(){j.removeEventListener("DOMContentLoaded",g,d);j.readyState="complete"},d);j.readyState="loading"}c.$script=function(p,m,o){var n=typeof m=="function"?m:(o||k),p=typeof p==l?[p]:p,r=typeof m==l?m:p.join(""),f=p.length,q=function(){if(!--f){e[r]=1;n();for(dset in a){b(dset.split("|"),function(s){return(s in e)})&&b(a[dset],function(s){s();a[dset].shift()})}}};b(p,function(s){setTimeout(function(){var u=j.createElement("script"),t=d;u.onload=u.onreadystatechange=function(){if((u.readyState&&u.readyState!=="complete"&&u.readyState!=="loaded")||t){return d}u.onload=u.onreadystatechange=null;t=true;q()};u.async=true;u.src=s;h.insertBefore(u,h.firstChild)},0);return true});return c};$script.ready=function(o,m,n){n=n||k;o=(typeof o==l)?[o]:o;var f=[];i(o,function(p){(p in e)||f.push(p)})&&b(o,function(p){return(p in e)})?m():(function(p){a[p]=a[p]||[];a[p].push(m);n(f)}(o.join("|")));return $script}}(window,document));
+    /**
+     * scripts.js
+     */
+    (function(c,j){var h=j.getElementsByTagName("script")[0],e={},a={},d=false,k=function(){},l="string",b=function(){return Array.every||function(f,p,o){for(var n=0,m=f.length;n<m;++n){if(!p(f[n],n,f)){return d}}return 1}}(),i=function(f,n,m){b(f,function(q,p,o){n(q,p,o);return true},m)};if(j.readyState==null&&j.addEventListener){j.addEventListener("DOMContentLoaded",function g(){j.removeEventListener("DOMContentLoaded",g,d);j.readyState="complete"},d);j.readyState="loading"}c.$script=function(p,m,o){var n=typeof m=="function"?m:(o||k),p=typeof p==l?[p]:p,r=typeof m==l?m:p.join(""),f=p.length,q=function(){if(!--f){e[r]=1;n();for(dset in a){b(dset.split("|"),function(s){return(s in e)})&&b(a[dset],function(s){s();a[dset].shift()})}}};b(p,function(s){setTimeout(function(){var u=j.createElement("script"),t=d;u.onload=u.onreadystatechange=function(){if((u.readyState&&u.readyState!=="complete"&&u.readyState!=="loaded")||t){return d}u.onload=u.onreadystatechange=null;t=true;q()};u.async=true;u.src=s;h.insertBefore(u,h.firstChild)},0);return true});return c};$script.ready=function(o,m,n){n=n||k;o=(typeof o==l)?[o]:o;var f=[];i(o,function(p){(p in e)||f.push(p)})&&b(o,function(p){return(p in e)})?m():(function(p){a[p]=a[p]||[];a[p].push(m);n(f)}(o.join("|")));return $script}}(window,document));
 
-    if(typeof jQuery === "undefined") {
-        $script('http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', function() {
-            jQuery.noConflict();
-            finalBeautyOfCode();
-        });
-    } else {
-        finalBeautyOfCode();
-    }
-
-    function finalBeautyOfCode() {
-	function path()
-	{
-		var args = arguments, result = [];
-
-		for(var i = 0; i < args.length; i++) {
-			result.push(args[i].replace('@', '<?php echo $pluginURL; ?>/scripts/'));
-		}
-		return result
-	};
-        (function($) {
-            $(document).ready(function() {
-		SyntaxHighlighter.autoloader.apply(null, path(
-			'applescript            @shBrushAppleScript.js',
-			'actionscript3 as3      @shBrushAS3.js',
-			'bash shell             @shBrushBash.js',
-			'coldfusion cf          @shBrushColdFusion.js',
-			'cpp c                  @shBrushCpp.js',
-			'c# c-sharp csharp      @shBrushCSharp.js',
-			'css                    @shBrushCss.js',
-			'delphi pascal          @shBrushDelphi.js',
-			'diff patch pas         @shBrushDiff.js',
-			'erl erlang             @shBrushErlang.js',
-			'groovy                 @shBrushGroovy.js',
-			'java                   @shBrushJava.js',
-			'jfx javafx             @shBrushJavaFX.js',
-			'js jscript javascript  @shBrushJScript.js',
-			'perl pl                @shBrushPerl.js',
-			'php                    @shBrushPhp.js',
-			'text plain             @shBrushPlain.js',
-			'py python              @shBrushPython.js',
-			'ruby rails ror rb      @shBrushRuby.js',
-			'sass scss              @shBrushSass.js',
-			'scala                  @shBrushScala.js',
-			'sql                    @shBrushSql.js',
-			'vb vbnet               @shBrushVb.js',
-			'xml xhtml xslt html    @shBrushXml.js'
-		));
-                SyntaxHighlighter.all();
-            });
-        })(jQuery);
-    }
+    $script(['<?php echo $pluginURL; ?>/scripts/shBrushAS3.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushBash.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushCSharp.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushColdFusion.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushCpp.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushCss.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushDelphi.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushDiff.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushErlang.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushGroovy.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushJScript.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushJava.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushJavaFX.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushPerl.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushPhp.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushPlain.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushPowerShell.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushPython.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushRuby.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushScala.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushSql.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushVb.js',
+        '<?php echo $pluginURL; ?>/scripts/shBrushXml.js'],
+        function() {
+            SyntaxHighlighter.config.clipboardSwf = '<?php echo $pluginURL; ?>/scripts/clipboard.swf';
+            SyntaxHighlighter.all();
+    });
 //]]>
 </script>
 <?php
--- plugins/HC_beautyOfCode/scripts/shBrushErlang.js
+++ plugins/HC_beautyOfCode/scripts/shBrushErlang.js
@@ -1,52 +1,54 @@
 /**
  * SyntaxHighlighter
- * http://alexgorbatchev.com/SyntaxHighlighter
+ * http://alexgorbatchev.com/
  *
  * SyntaxHighlighter is donationware. If you are using it, please donate.
- * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
+ * http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
  *
  * @version
- * 3.0.83 (July 02 2010)
+ * 2.1.382 (June 24 2010)
  * 
  * @copyright
- * Copyright (C) 2004-2010 Alex Gorbatchev.
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
  *
  * @license
- * Dual licensed under the MIT and GPL licenses.
+ * This file is part of SyntaxHighlighter.
+ * 
+ * SyntaxHighlighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SyntaxHighlighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SyntaxHighlighter.  If not, see <http://www.gnu.org/copyleft/lesser.html>.
  */
-;(function()
+SyntaxHighlighter.brushes.Erlang = function()
 {
-	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+	// Contributed by Jean-Lou Dupont
+	// http://jldupont.blogspot.com/2009/06/erlang-syntax-highlighter.html  
 
-	function Brush()
-	{
-		// Contributed by Jean-Lou Dupont
-		// http://jldupont.blogspot.com/2009/06/erlang-syntax-highlighter.html  
+	// According to: http://erlang.org/doc/reference_manual/introduction.html#1.5
+	var keywords = 'after and andalso band begin bnot bor bsl bsr bxor '+
+		'case catch cond div end fun if let not of or orelse '+
+		'query receive rem try when xor'+
+		// additional
+		' module export import define';
 
-		// According to: http://erlang.org/doc/reference_manual/introduction.html#1.5
-		var keywords = 'after and andalso band begin bnot bor bsl bsr bxor '+
-			'case catch cond div end fun if let not of or orelse '+
-			'query receive rem try when xor'+
-			// additional
-			' module export import define';
+	this.regexList = [
+		{ regex: new RegExp("[A-Z][A-Za-z0-9_]+", 'g'), 			css: 'constants' },
+		{ regex: new RegExp("\\%.+", 'gm'), 						css: 'comments' },
+		{ regex: new RegExp("\\?[A-Za-z0-9_]+", 'g'), 				css: 'preprocessor' },
+		{ regex: new RegExp("[a-z0-9_]+:[a-z0-9_]+", 'g'), 			css: 'functions' },
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },
+		{ regex: new RegExp(this.getKeywords(keywords),	'gm'),		css: 'keyword' }
+		];
+};
 
-		this.regexList = [
-			{ regex: new RegExp("[A-Z][A-Za-z0-9_]+", 'g'), 			css: 'constants' },
-			{ regex: new RegExp("\\%.+", 'gm'), 						css: 'comments' },
-			{ regex: new RegExp("\\?[A-Za-z0-9_]+", 'g'), 				css: 'preprocessor' },
-			{ regex: new RegExp("[a-z0-9_]+:[a-z0-9_]+", 'g'), 			css: 'functions' },
-			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },
-			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },
-			{ regex: new RegExp(this.getKeywords(keywords),	'gm'),		css: 'keyword' }
-			];
-	};
-
-	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['erl', 'erlang'];
-
-	SyntaxHighlighter.brushes.Erland = Brush;
-
-	// CommonJS
-	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
-})();
+SyntaxHighlighter.brushes.Erlang.prototype	= new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Erlang.aliases	= ['erl', 'erlang'];
